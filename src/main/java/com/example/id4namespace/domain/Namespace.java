53800b294e070e16fc739eccfdcc773032d0bdea
Eduardo Aranda <edar1986@gmail.com>
2020-11-01 15:37:34 -0300

    Adding docker supports

--- src/main/java/com/example/id4namespace/domain/Namespace.java
+++ src/main/java/com/example/id4namespace/domain/Namespace.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 
 @Document
 @Data
@@ -17,5 +17,6 @@ public class Namespace {
 
     @Id
     private String id;
+    @Indexed
     private String name;
 }
---
+++ src/main/java/com/example/id4namespace/service/NamespaceServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.id4namespace.service;
+
+import com.example.id4namespace.domain.Namespace;
+import com.example.id4namespace.repository.NamespaceRepository;
+import com.example.id4namespace.service.dto.NamespaceCreateDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NamespaceServiceImpl implements NamespaceService {
+
+    private final NamespaceRepository repository;
+    private final IdGenerator generator;
+
+    public Namespace createNamespace(NamespaceCreateDTO namespaceDTO) {
+        return repository.save(Namespace.builder()
+                .id(generator.generateId(namespaceDTO.getName()))
+                .name(namespaceDTO.getName())
+                .build());
+    }
+
+}
